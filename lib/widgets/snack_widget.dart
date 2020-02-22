94b12241530b9cba3069eb77ccbb78129bc153fd
Godsend Joseph <bobbyjoseph108@gmail.com>
2020-02-22 14:58:52 +0100

    fixed snack bar state issue

--- lib/widgets/snack_widget.dart
+++ lib/widgets/snack_widget.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/material.dart';
 
 class SnackWidget extends StatelessWidget {
+  final int index;
+  final List items;
+
+  SnackWidget({this.index, this.items});
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: SnackBar(
-          content: Text("Working Snack Bar"),
+      child: Dismissible(
+        key: Key(items[index]),
+        direction: DismissDirection.startToEnd,
+        background: Container(
+          color: Colors.red,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Icon(
+                  Icons.delete,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
         ),
-      );
+        onDismissed: (direction) {
+          items.removeAt(index);
+          Scaffold.of(context).showSnackBar(new SnackBar(
+            content: Text("Deleted"),
+          ));
+        },
+        child: ListTile(
+          title: Text("${items[index]}"),
+        ),
+      ),
+    );
   }
 }
